1d9ae4213cbe4f55f51f761dd5f7d23cf2f0a75d
Iman GhashghaeiZadeh <44636790+imangz@users.noreply.github.com>
2020-09-05 15:57:55 +0430

    Update iGzFaceToolKit.py
    A toolkit to use in a main program, which contain a functional wrenches like lbp() to hain LBP-Feature-Extraction, or hog() to gain HOG-Feature-extraction.

--- iGzFaceToolKit.py
+++ iGzFaceToolKit.py
@@ -27,8 +27,6 @@ def   cropFaces_save_FromImage(path, imageName, writeAddress):
         cv2.imwrite(pathToWrite, crop_img)
 
 
-
-
 #a function to cpature, crop ans save images simultaneously from a movie
 def capture_cropFaces_save_FromMovie(path, movieName, writeAddress):
     vidCap = cv2.VideoCapture(path+movieName)
@@ -110,4 +108,77 @@ def hog(img, bin_n = 16):
     hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
     hist = np.hstack(hists)     # hist is a 64 bit vector
     return hist
+
+#declare a function to get score from a model
+def get_modelScore(model, xTrain, xTest, yTrain, yTest,printScore = False):
+    model.fit(xTrain,yTrain)
+    score = model.score(xTest, yTest)
+    if (printScore == True):
+        print(model.score(xTest, yTest))
+    return score
+
+
+def get_cross_val_score(model, x, y, k_to_fold = 3):
+    folds = KFold(n_splits = k_to_fold)
+    score_history = []
+    # calculate k-fold cross validation scores in a loop
+    for trainIndex, testIndex in folds.split(x):
+        x_train, x_test, y_train, y_test = x[trainIndex], x[testIndex], y[trainIndex], y[testIndex]
+        score_history.append(get_modelScore(model, x_train, x_test, y_train, y_test,printScore=True))
+        print(score_history)
+    return score_history
+
+
+def get_pixel(img, center, x, y):
+    new_value = 0
+    try:
+        # If local neighbourhood pixel value is greater than or equal to center pixel values then set it to 1
+        if img[x][y] >= center:
+            new_value = 1
+    except:
+        # Exception is required when neighbourhood value of a center pixel value is null i.e. values present at boundaries.
+        pass
+
+    return new_value
+
+
+def lbp_calculated_pixel(img, x, y):
+    center = img[x][y]
+    val_ar = []
+    # top_left
+    val_ar.append(get_pixel(img, center, x - 1, y - 1))
+    # top
+    val_ar.append(get_pixel(img, center, x - 1, y))
+    # top_right
+    val_ar.append(get_pixel(img, center, x - 1, y + 1))
+    # right
+    val_ar.append(get_pixel(img, center, x, y + 1))
+    # bottom_right
+    val_ar.append(get_pixel(img, center, x + 1, y + 1))
+    # bottom
+    val_ar.append(get_pixel(img, center, x + 1, y))
+    # bottom_left
+    val_ar.append(get_pixel(img, center, x + 1, y - 1))
+    # left
+    val_ar.append(get_pixel(img, center, x, y - 1))
+    # Now, we need to convert binary values to decimal
+    power_val = [1, 2, 4, 8, 16, 32, 64, 128]
+    val = 0
+
+    for i in range(len(val_ar)):
+        val += val_ar[i] * power_val[i]
+
+    return val
+
+def lbp(imagePath):
+    img_bgr = cv2.imread(imagePath, 1)
+    height, width, _ = img_bgr.shape
+    # We need to convert RGB image into gray one because gray image has one channel only.
+    img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+    # Create a numpy array as the same height and width of RGB image
+    img_lbp = np.zeros((height, width), np.uint8)
+    for i in range(0, height):
+        for j in range(0, width):
+            img_lbp[i, j] = lbp_calculated_pixel(img_gray, i, j)
+    return img_lbp
 
